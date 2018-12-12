b38dcebdef6a1226051a9767ff45975d511aa399
Douglas Bowman <d.s.bowman88@gmail.com>
2018-12-12 13:58:57 +0100

    JS code improved
    Edited parent().parent().parent(). into parentuntil(".row")

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
     $(".card-title").filter(function() {
-      $(this).parent().parent().parent().toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      $(this).parentsUntil(".row").toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
 });
