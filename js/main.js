54bf427444fa3680e0264631964e3c7334c3efa0
Douglas Bowman <d.s.bowman88@gmail.com>
2018-12-14 11:52:35 +0100

    Added descending into sort function

--- js/main.js
+++ js/main.js
@@ -10,21 +10,36 @@ $(document).ready(function(){
 function sortByName(){
   list = document.getElementsByClassName("card-title"); // get all card-titles
   card = document.getElementsByClassName("card"); // get all cards
-  cardcol = document.getElementsByClassName("col-sm-6 col-md-4 col-lg-4")
-  console.log(cardcol)
-  switching = true
+  cardcol = document.getElementsByClassName("col-sm-6 col-md-4 col-lg-4");
+  switchcount = 0;
+  switching = true;
+  dir = "asc";
   while (switching){
     switching = false
     for (i = 0; i < (list.length - 1); i++) {
       shouldSwitch = false;
+      if (dir == "asc") {
       if (list[i].innerText.toLowerCase() > list[i + 1].innerText.toLowerCase()){
-        shouldSwitch = true
-        break
+        shouldSwitch = true;
+        break;
       }
     }
-    if (shouldSwitch){
-      cardcol[i].parentNode.insertBefore(cardcol[i + 1], cardcol[i]);
-      switching = true
+    else if (dir == "desc"){
+      if (list[i].innerText.toLowerCase() < list[i + 1].innerText.toLowerCase()){
+        shouldSwitch = true;
+        break;
+      }
     }
   }
+    if (shouldSwitch){
+      cardcol[i].parentNode.insertBefore(cardcol[i + 1], cardcol[i]);
+      switching = true;
+      switchcount ++;
+    } else {
+      if (switchcount == 0 && dir == "asc"){
+        dir = "desc";
+        switching = true;
+    }
+  }
+}
 }
